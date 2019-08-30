79b4a6e2abacf97a47566d87ceaa313a0d82501c
yangchaoy259189888 <17792035609@163.com>
2019-08-30 20:03:46 +0800

    最长公共前缀

---
+++ src/com/offer/LongestCommonPrefix.java
@@ -0,0 +1,33 @@
+package com.offer;
+
+import java.util.Scanner;
+
+/**
+ * @author 西邮陈冠希
+ * @date 2019/8/30
+ **/
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String res = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < strs[i].length() && j < res.length(); j++) {
+                if (res.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
+            }
+            res = res.substring(0, j);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strs = {"flower","flow","flight"};
+        String s = longestCommonPrefix(strs);
+        System.out.println(s);
+    }
+}
