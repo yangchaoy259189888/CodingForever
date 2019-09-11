76d3ac20b45efb75acefa7891e6fddf1a3fafe09
yangchaoy259189888 <17792035609@163.com>
2019-09-11 09:01:49 +0800

    最大子序和

---
+++ src/com/offer/simple/Maximum_Subarray.java
@@ -0,0 +1,28 @@
+package com.offer.simple;
+
+/**
+ * @author 西邮陈冠希
+ * @date 2019/9/10
+ **/
+public class Maximum_Subarray {
+    public static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(res, sum);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int i = maxSubArray(nums);
+        System.out.println(i);
+    }
+}
